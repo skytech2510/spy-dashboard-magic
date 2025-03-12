45f24ed9ca2830bce3c366f29a8135adfd5f9b70
SmartCryptoDev <lukegreen1004@gmail.com>
2025-03-13 01:32:26 +0700

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { AuthProvider } from "@/context/AuthContext";
-// import { ThemeProvider } from "@/context/ThemeContext";
-// import Index from "./pages/Index";
-// import Login from "./pages/Login";
-// import NotFound from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/context/AuthContext";
+import { ThemeProvider } from "@/context/ThemeContext";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
